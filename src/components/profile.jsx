8fc24e1770d91634e3c481abe599c25e7ba5e09c
Whirmon <whirmon@yahoo.com>
2023-06-28 11:54:16 -0600

    prueba 4
    Agregar estilo y finalizar log in/out auth0 con perfil

--- src/components/profile.jsx
+++ src/components/profile.jsx
@@ -1,28 +1,26 @@
-import React from 'react';
+import { useAuth0 } from "@auth0/auth0-react";
+import React from "react";
 
-class PerfilDeUsuario extends React.Component {
-  constructor(props) {
-    super(props);
+const Profile = () => {
+  const { user, isAuthenticated, isLoading } = useAuth0();
 
-    this.state = {
-      nombre: 'Jose Juarez',
-      edad: 25,
-      email: 'josejuarez@ejemplo.com'
-    };
+  if (isLoading) {
+    return <div>Loading ...</div>;
   }
 
-  render() {
-    const { nombre, edad, email } = this.state;
+  return (
+    isAuthenticated && (
+    <div className='container container-sm container-md container-lg container-xl container-xxl'>
+        <div className="card">
+        <div className="card-body">
+        <img src={user.picture} alt={user.name} />
+        <h5 className="card-title"><strong>Nombre:</strong> {user.name}</h5>
+        <p className="card-text"><strong>Email:</strong> {user.email}</p>
+    </div>
+    </div>
+    </div>
+    )
+  );
+};
 
-    return (
-      <div className='container'>
-        <h2>Perfil de Usuario</h2>
-        <p><strong>Nombre:</strong> {nombre}</p>
-        <p><strong>Edad:</strong> {edad}</p>
-        <p><strong>Email:</strong> {email}</p>
-      </div>
-    );
-  }
-}
-
-export default PerfilDeUsuario;
+export default Profile;
